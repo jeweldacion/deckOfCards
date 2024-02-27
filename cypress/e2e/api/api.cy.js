3873821fcd33db353b8c448b1f323afe5d6e8a32
Jewel Dacion <dacionjewelshayne01@gmail.com>
2024-02-27 09:30:36 -0500

    refractor test cases

--- cypress/e2e/api/api.cy.js
+++ cypress/e2e/api/api.cy.js
@@ -1,43 +1,71 @@
 describe('Deck of Cards API', () => {
-    let deckId;
+    let deckId, player1Cards, player2Cards;
+
     it('Navigate to Deck of cards URL', () => {
         cy.visit('/');
     });
-    //Application is up and running
-    it('Should be up', async () => {
-        const response = await cy.request('GET', '/');
-        expect(response.status).to.equal(200);
-    });
-    //Get a new deck of cards
 
-    it('Should get a new deck', async () => {
-        const response = await cy.request('GET', '/api/deck/new/');
-        expect(response.status).to.equal(200);
-        expect(response.body.success).to.equal(true);
-        deckId = response.body.deck_id;
+    it('Should be up and running', () => {
+        cy.request('GET', '/').then((response) => {
+            expect(response.status).to.equal(200);
+        });
     });
-    //Shuffling the new deck of cards
-    it('Should shuffle the deck', async () => {
-        const response = await cy.request(
-            'GET',
-            `/api/deck/${deckId}/shuffle/?deck_count=1`
+
+    it('Should get a new deck', () => {
+        cy.request('GET', '/api/deck/new/').then((response) => {
+            expect(response.status).to.equal(200);
+            expect(response.body.success).to.equal(true);
+            deckId = response.body.deck_id;
+        });
+    });
+
+    it('Should shuffle the deck', () => {
+        cy.request('GET', `/api/deck/${deckId}/shuffle/?deck_count=1`).then(
+            (response) => {
+                expect(response.status).to.equal(200);
+                expect(response.body.shuffled).to.equal(true);
+            }
         );
-        expect(response.status).to.equal(200);
-        expect(response.body.shuffled).to.equal(true);
+
+        it('Deal three cards to each of two players and check for blackjack', () => {
+            cy.request(`GET`, `/api/deck/${deckId}/draw/?count=6`).then(
+                (response) => {
+                    player1Cards = response.body.cards.slice(0, 3);
+                    player2Cards = response.body.cards.slice(3, 6);
+
+                    checkForBlackjack('Player 1', player1Cards);
+                    checkForBlackjack('Player 2', player2Cards);
+                }
+            );
+        });
+
+        // Custom Cypress command to check for blackjack
+        Cypress.Commands.add('checkForBlackjack', (player, cards) => {
+            const hasBlackJack = (cards) => {
+                if (cards.length === 2) {
+                    const firstCardValue = cards[0].value;
+                    const secondCardValue = cards[1].value;
+
+                    return (
+                        (firstCardValue === 'ACE' &&
+                            ['10', 'JACK', 'QUEEN', 'KING'].includes(
+                                secondCardValue
+                            )) ||
+                        (secondCardValue === 'ACE' &&
+                            ['10', 'JACK', 'QUEEN', 'KING'].includes(
+                                firstCardValue
+                            ))
+                    );
+                }
+
+                return false;
+            };
+
+            cy.then(() => {
+                if (hasBlackJack(cards)) {
+                    cy.log(`${player} has blackjack!`);
+                }
+            });
+        });
     });
-    //Deal 3 cards for each player
-    // let player1Cards, player2Cards;
-    // it('Should deal three cards to each of two players', async () => {
-    //     const response = await cy.request(
-    //         'GET',
-    //         `/api/deck/${deckId}/draw/?count=6`
-    //     );
-
-    //     player1Cards = response.body.cards.slice(0, 3);
-    //     player2Cards = response.body.cards.slice(3, 6);
-    // });
-
-    // const hasBlackJack = (cards) => {};
-
-    // it('should check whether either player has blackjack', async () => {});
 });
